dc7576e4047417c84e6dde5581d6910dba0b6c70
Adrian Chong <adrian.shun1387@outlook.com>
2025-06-22 22:59:30 +0930

    env file patch

--- main.py
+++ main.py
@@ -4,28 +4,32 @@ from google import genai
 from google.genai import types
 import sys
 
-if len(sys.argv) < 2:
-    print("No prompt provided.")
-    sys.exit(1)
+def main():
+    if len(sys.argv) < 2:
+        print("No prompt provided.")
+        sys.exit(1)
 
 
 
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
 
-user_prompt = sys.argv[1]
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-]
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
-# contents = input("Contents for LLM: ")
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    # contents = input("Contents for LLM: ")
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 
-if "--verbose" in sys.argv:
-    #print(response.text)
-    print("User prompt:", user_prompt)
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if "--verbose" in sys.argv:
+        print("User prompt:", user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print(response.text)
+
+if __name__ == "__main__":
+    main()
