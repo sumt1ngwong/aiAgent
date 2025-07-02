31db5c016281fc86d7930c29eae9407ce2dcbd74
Adrian Chong <adrian.shun1387@outlook.com>
2025-07-02 17:50:43 +0930

    get file content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,60 +1,49 @@
 import os 
 
 def get_files_info(working_directory, directory=None):
-    if os.path.commonpath([os.path.abspath(working_directory), os.path.abspath(f"{working_directory}/{directory}")]) != os.path.abspath(working_directory):
+    full_path = os.path.join(working_directory, directory)
+
+    if os.path.commonpath([os.path.abspath(working_directory), os.path.abspath(full_path)]) != os.path.abspath(working_directory):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    elif os.path.isdir(f"{working_directory}/{directory}") == False:
+    elif os.path.isdir(full_path) == False:
         return f'Error: "{directory}" is not a directory'
     else:
-        path = os.path.abspath(f"{working_directory}/{directory}")
-        contents = os.listdir(path)
-        results = []
+        try:
+            path = os.path.abspath(full_path)
+            contents = os.listdir(path)
+            results = []
 
-        for content in contents:
-            full_path = os.path.join(path, content)
-            file_size = os.path.getsize(full_path)
-            is_dir = os.path.isdir(full_path)
-            results.append(f"{content}: file_size={file_size} bytes, is_dir={is_dir}")
+            for content in contents:
+                full_path_c = os.path.join(path, content)
+                file_size = os.path.getsize(full_path_c)
+                is_dir = os.path.isdir(full_path_c)
+                results.append(f"{content}: file_size={file_size} bytes, is_dir={is_dir}")
 
-        return "\n".join(results) if results else f'"{directory}" is an empty directory.'
+            return "\n".join(results) if results else f'"{directory}" is an empty directory.'
+        except Exception as e:
+            return f"Error: {e}"
 
+def get_file_content(working_directory, file_path):
+    full_path = os.path.join(working_directory, file_path)
 
+    if os.path.commonpath([os.path.abspath(working_directory), os.path.abspath(full_path)]) != os.path.abspath(working_directory):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    elif os.path.isfile(full_path) == False:
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    else:
+        try:
+            maximum_characters = 10000
+            with open(full_path, "r") as f:
+                content = f.read()
+                character_count = len(content)
 
-print(get_files_info("calculator", "../"))
-
-                        
-                    
-
-# print(os.path.abspath("calculator"))
-# print(os.path.abspath("calculator/pkg"))
-# print(os.path.commonpath([os.path.abspath("calculator"), os.path.abspath("calculator/pkg")]))
-
-
-# CHECK COMMON PATH LOGICCC FUCK MEE. CUNT 
-
-
-# path = os.path.abspath("calculator") ## variable should be directory
-# isdir = os.path.isdir("pkg")
-
-# directory = os.listdir(path)
-# x = os.path.getsize("calculator/tests.py")
-# y = os.path.isfile("calculator/tests.py")
-
-# # print(x)
-# # print(y)
-# # print(path)
-# # print(isdir)
-# # print(contents)
-# print(directory)
-
-# # for content in directory:
-# #     if os.path.isdir(os.path.join(path, content)) == True:
-# #         file_size = os.path.getsize(os.path.join(path, content))
-# #         is_dir = os.path.isdir(os.path.join(path, content))
-# #         print(f"{content}: file_size={file_size} bytes, is_dir={is_dir}")
-# #     elif os.path.isfile(os.path.join(path, content)) == True:
-# #         file_size = os.path.getsize(os.path.join(path, content))
-# #         is_dir = os.path.isdir(os.path.join(path, content))
-# #         print(f"{content}: file_size={file_size} bytes, is_dir={is_dir}")
-
-
+            if character_count > maximum_characters:
+                with open(full_path, "r") as f:
+                    file_content_string = f.read(maximum_characters)
+                return f'{file_content_string}[...File "{file_path}" truncated at 1000 characters]'
+            else:
+                with open(full_path, "r") as f:
+                    file_content_string = f.read()
+                return file_content_string
+        except Exception as e:
+            f"Error: {e}"
--- tests.py
+++ tests.py
@@ -1,12 +1,15 @@
 
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, get_file_content
 
 def test_func():
-    print(get_files_info("calculator", "."))
-    print(get_files_info("calculator", "pkg"))
-    print(get_files_info("calculator", "/bin"))
-    print(get_files_info("calculator", "../"))
-
+    # print(get_files_info("calculator", "."))
+    # print(get_files_info("calculator", "pkg"))
+    # print(get_files_info("calculator", "/bin"))
+    # print(get_files_info("calculator", "../"))
+    # print(get_file_content("calculator", "lorem.txt"))
+    print(get_file_content("calculator", "main.py"))
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print(get_file_content("calculator", "/bin/cat"))
 
 test_func()
 
