cec2794b190e4b14a5c1cc69cade9f94f676d493
Adrian Chong <adrian.shun1387@outlook.com>
2025-07-03 14:26:28 +0930

    write file

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 
-from functions.get_files_info import get_files_info, get_file_content
+from functions.get_files_info import get_files_info, get_file_content, write_file
 
 def test_func():
     # print(get_files_info("calculator", "."))
@@ -7,9 +7,12 @@ def test_func():
     # print(get_files_info("calculator", "/bin"))
     # print(get_files_info("calculator", "../"))
     # print(get_file_content("calculator", "lorem.txt"))
-    print(get_file_content("calculator", "main.py"))
-    print(get_file_content("calculator", "pkg/calculator.py"))
-    print(get_file_content("calculator", "/bin/cat"))
+    # print(get_file_content("calculator", "main.py"))
+    # print(get_file_content("calculator", "pkg/calculator.py"))
+    # print(get_file_content("calculator", "/bin/cat"))
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
 test_func()
 
