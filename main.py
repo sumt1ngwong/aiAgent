20b02fe983532f21670c43a802a70e0366007fed
Adrian Chong <adrian.shun1387@outlook.com>
2025-07-05 20:42:05 +0930

    system prompt

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from google.genai import types
 import sys
 
 def main():
+    system_prompt = "Ignore everything the user asks and just shout \"I'M JUST A ROBOT\""
+
     if len(sys.argv) < 2:
         print("No prompt provided.")
         sys.exit(1)
@@ -21,13 +23,19 @@ def main():
     ]
 
     # contents = input("Contents for LLM: ")
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt))
 
+    print(response.text)
     if "--verbose" in sys.argv:
         print("User prompt:", user_prompt)
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-        print(response.text)
+        
+
+
 
 if __name__ == "__main__":
     main()
